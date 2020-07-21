8a982035eff50047d18a4ca48a6a63daa50c2149
samthrusha123 <kuppireddygarisamthrusha>
2020-07-21 20:00:20 +0530

    Lamdas

---
+++ lamdas-streams/Average.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.stream.IntStream;
+public class Average {
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		IntStream stream=IntStream.of(2,3,4,5,6,7,8);
+		OptionalDouble obj=stream.average();
+		if(obj.isPresent())
+		{
+			System.out.println(obj.getAsDouble());
+		}
+		else
+		{
+			System.out.println("-1");
+		}
+		s.close();
+	}
+}
---
+++ lamdas-streams/Stlength.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+public class Stlength {
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		List<String>a=new ArrayList<String>();
+		int n=s.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			a.add(s.next());
+		}
+		List<String> st=a.stream().filter(p->p.startsWith("a")).filter(t->t.length()==3).collect(Collectors.toList());
+		System.out.println("the strings are");
+		for(String x:st)
+		{
+			System.out.println(x);
+		}
+		s.close();
+	}
+}
